0ccbf0665c22a676c84514be1a0f54d00bdffde7
vihanga-liyanage <vihangaliyanage007@gmail.com>
2020-01-27 18:19:31 +0530

    Upgrade to spring 2

--- sample-by-alonsegal/src/main/java/com/alonsegal/config/HibernateConfig.java
+++ sample-by-alonsegal/src/main/java/com/alonsegal/config/HibernateConfig.java
@@ -4,26 +4,21 @@ import org.hibernate.MultiTenancyStrategy;
 import org.hibernate.cfg.Environment;
 import org.hibernate.context.spi.CurrentTenantIdentifierResolver;
 import org.hibernate.engine.jdbc.connections.spi.MultiTenantConnectionProvider;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.orm.jpa.JpaProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.orm.jpa.JpaVendorAdapter;
 import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
 import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
 
-import javax.sql.DataSource;
 import java.util.HashMap;
 import java.util.Map;
+import javax.sql.DataSource;
 
 /**
  * Created by Alon Segal on 23/03/2017.
  */
 @Configuration
 public class HibernateConfig {
-
-    @Autowired
-    private JpaProperties jpaProperties;
 
     @Bean
      public JpaVendorAdapter jpaVendorAdapter() {
@@ -35,10 +30,11 @@ public class HibernateConfig {
                                                                        MultiTenantConnectionProvider multiTenantConnectionProviderImpl,
                                                                        CurrentTenantIdentifierResolver currentTenantIdentifierResolverImpl) {
         Map<String, Object> properties = new HashMap<>();
-        properties.putAll(jpaProperties.getHibernateProperties(dataSource));
         properties.put(Environment.MULTI_TENANT, MultiTenancyStrategy.SCHEMA);
         properties.put(Environment.MULTI_TENANT_CONNECTION_PROVIDER, multiTenantConnectionProviderImpl);
         properties.put(Environment.MULTI_TENANT_IDENTIFIER_RESOLVER, currentTenantIdentifierResolverImpl);
+        properties.put(org.hibernate.cfg.Environment.SHOW_SQL, true);
+        properties.put(org.hibernate.cfg.Environment.FORMAT_SQL, true);
 
         LocalContainerEntityManagerFactoryBean em = new LocalContainerEntityManagerFactoryBean();
         em.setDataSource(dataSource);
